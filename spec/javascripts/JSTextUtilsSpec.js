263462e6338ea383ef650148e7e4b747cd35cddb
Prateek Dayal <prateek@muziboo.com>
2011-06-13 16:00:09 +0530

    forgot to add the spec file

---
+++ spec/javascripts/JSTextUtilsSpec.js
@@ -0,0 +1,94 @@
+// DateJS is used for testing some of the stuff
+
+describe("JSUtils", function() {
+
+  describe("JSUtils.Date", function() {
+
+    describe("isToday", function() {
+      it("should tell if the date is not today's date", function() {
+        var yday = (1).days().ago();
+        expect(JSUtils.Date.isToday(yday)).toEqual(false);
+      });  
+      
+      it("should tell if the date is today's date", function() {
+        var now = Date.now();
+        expect(JSUtils.Date.isToday(now)).toEqual(true);
+      });  
+
+    });  
+
+    describe("isThisYear", function() {
+      
+      it("should tell if the date is not this year", function() {
+        var lastYear = (1).year().ago();
+        expect(JSUtils.Date.isThisYear(lastYear)).toEqual(false);
+      });
+      
+      it("should tell if the date is this year", function() {
+        var now = Date.now();
+        expect(JSUtils.Date.isThisYear(now)).toEqual(true);
+      });  
+
+    });  
+    
+    
+    
+   describe("Today's timestamps", function() {
+
+     //beforeEach(function() {
+       //this.isTodayMock = sinon.mock(JSUtils.Date);
+
+       //// Lets assume all dates are today's
+       //isTodayMock.expects('isToday').once().returns(true);
+     //});
+
+     //afterEach(function() { 
+       //// Everything should have used isToday
+       //isTodayMock.verify();
+       //this.isTodayMock.restore();
+     //}
+     
+     
+     it("should show 0 - 12 hours today as AM", function(){
+       var dateNow = Date.now();
+       var amTime = new Date(dateNow.getFullYear(),
+                             dateNow.getMonth(),
+                             dateNow.getDate(),
+                             2, 49, 0, 0);
+       expect(JSUtils.Date.toHuman(amTime)).toEqual('2:49 am');
+     });
+     
+     it("should show 13 - 23 hours today as AM", function(){
+       var dateNow = Date.now();
+       var amTime = new Date(dateNow.getFullYear(),
+                             dateNow.getMonth(),
+                             dateNow.getDate(),
+                             12, 49, 0, 0);
+       expect(JSUtils.Date.toHuman(amTime)).toEqual('12:49 pm');
+     });
+     
+     it("should show 13 - 23 hours today as AM", function(){
+       var dateNow = Date.now();
+       var amTime = new Date(dateNow.getFullYear(),
+                             dateNow.getMonth(),
+                             dateNow.getDate(),
+                             23, 49, 0, 0);
+       expect(JSUtils.Date.toHuman(amTime)).toEqual('11:49 pm');
+     });
+     
+     it("should show any date in the last year as dd mm", function(){
+       var dateNow = Date.now();
+       var amTime = new Date(dateNow.getYear(),
+                             5,
+                             10,
+                             23, 49, 0, 0);
+       expect(JSUtils.Date.toHuman(amTime)).toEqual('11:49 pm');
+     });
+   });  
+     
+
+  });  
+  
+  
+});  
+
